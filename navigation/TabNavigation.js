0c5933e18b2489a3ed0c4bee45028c1272c3954d
Sungho Yun <itlockit@gmail.com>
2019-11-30 18:59:37 +0900

    custom icon bottom navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,23 +1,57 @@
-import {createAppContainer} from "react-navigation";
-import {createBottomTabNavigator} from "react-navigation-tabs"
+import React from "react";
+import { Platform } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import MovieScreen from "../screens/Movies";
 import TVScreen from "../screens/Tv";
 import SearchScreen from "../screens/Search";
-import {BG_COLOR} from "../constants/Color";
+import { BG_COLOR } from "../constants/Color";
+import TabBarIcon from "../components/TabBarIcon";
 
 const TabNavigation = createBottomTabNavigator(
-    {
-        Movies : MovieScreen,
-        TV : TVScreen,
-        Search : SearchScreen
-    },{
-        tabBarOptions:{
-            showLabel : false,
-            style:{
-                backgroundColor: BG_COLOR
-            }
-        }
+  {
+    Movie: {
+      screen: MovieScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+          />
+        )
+      }
+    },
+    Tv: {
+      screen: TVScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+          />
+        )
+      }
     }
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      style: {
+        backgroundColor: BG_COLOR
+      }
+    }
+  }
 );
 
 export default createAppContainer(TabNavigation);
